0da2e5aa1407f9aaf88bd5269b01e1dde4584b9d
Alena <ABelova0605@gmail.com>
2021-06-09 00:39:02 +0300

    correct code in hw3-4.py

--- hw3-4.py
+++ hw3-4.py
@@ -15,7 +15,7 @@ def my_func(x, y):
     return x ** y
 
 
-print(my_func(2, -4))
+print(my_func(2, -3))
 
 
 # Второй вариант: цикл
@@ -27,4 +27,4 @@ def my_func(x, y):
     return 1 / res
 
 
-print(my_func(2, -4))
+print(my_func(2, -3))
